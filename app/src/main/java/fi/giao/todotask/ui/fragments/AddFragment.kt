1f347af5509a57b7170a4b99232fe70e0ab8d26d
Giao Ngo <giao.ngo@metropolia.fi>
2022-07-08 11:35:08 +0300

    Initialize the button add and collect input data from user

--- app/src/main/java/fi/giao/todotask/ui/fragments/AddFragment.kt
+++ app/src/main/java/fi/giao/todotask/ui/fragments/AddFragment.kt
@@ -5,16 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import fi.giao.todotask.R
+import fi.giao.todotask.ToDoActivity
+import fi.giao.todotask.databinding.FragmentAddBinding
+import fi.giao.todotask.db.Task
+import fi.giao.todotask.model.TaskViewModel
+import kotlinx.android.synthetic.main.fragment_add.*
 
-class AddFragment : Fragment() {
+class AddFragment : Fragment(){
+    private lateinit var binding: FragmentAddBinding
+    private lateinit var viewModel : TaskViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        viewModel =  (activity as ToDoActivity).viewModel
+        binding = FragmentAddBinding.inflate(inflater,container,false)
+        val myAdapter = ArrayAdapter<String>(requireActivity(),android.R.layout.simple_spinner_dropdown_item,resources.getStringArray(R.array.task_priorities))
+        binding.apply {
+            prioritySpinner.adapter = myAdapter
+            addButton.setOnClickListener {
+                val taskText =  filledTaskText.editText!!.text
+                val priority = prioritySpinner.selectedItemPosition
+                val timeStamp = System.currentTimeMillis()
+                viewModel.upsertTask(Task(0,taskText.toString(),priority,timeStamp))
+                findNavController().navigate(R.id.action_addFragment_to_taskFragment)
+            }
+        }
+        return binding.root
     }
-
 
 }

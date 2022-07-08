4a6517c7ef1077e66bdca6b7fa89023dea5a066b
Giao Ngo <giao.ngo@metropolia.fi>
2022-07-08 11:32:19 +0300

    Initialize ViewModel in ToDoActivity.kt

--- app/src/main/java/fi/giao/todotask/ToDoActivity.kt
+++ app/src/main/java/fi/giao/todotask/ToDoActivity.kt
@@ -2,12 +2,22 @@ package fi.giao.todotask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
+import fi.giao.todotask.db.TaskDatabase
+import fi.giao.todotask.model.TaskViewModel
+import fi.giao.todotask.repository.Repository
+import fi.giao.todotask.ui.fragments.TaskViewModelProviderFactory
 
 class ToDoActivity : AppCompatActivity() {
+    lateinit var viewModel: TaskViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val repository = Repository(TaskDatabase.getDatabase(this).taskDao())
+        val viewModelProviderFactory = TaskViewModelProviderFactory(application,repository)
+        viewModel = ViewModelProvider(this,viewModelProviderFactory).get(TaskViewModel::class.java)
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navHostFragment.navController.navigate(R.id.taskFragment)
     }

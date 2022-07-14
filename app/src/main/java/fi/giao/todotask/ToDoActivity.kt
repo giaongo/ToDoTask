3c9ec000dd934292cd87a89490aafbd5b454bcb0
Giao Ngo <giao.ngo@metropolia.fi>
2022-07-15 00:03:14 +0300

    Set up action button on menu action bar

--- app/src/main/java/fi/giao/todotask/ToDoActivity.kt
+++ app/src/main/java/fi/giao/todotask/ToDoActivity.kt
@@ -3,7 +3,9 @@ package fi.giao.todotask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import fi.giao.todotask.db.TaskDatabase
 import fi.giao.todotask.model.TaskViewModel
 import fi.giao.todotask.repository.Repository
@@ -19,6 +21,13 @@ class ToDoActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this,viewModelProviderFactory).get(TaskViewModel::class.java)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupActionBarWithNavController(this,navHostFragment.navController)
         navHostFragment.navController.navigate(R.id.taskFragment)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() ||  super.onSupportNavigateUp()
+    }
+
 }

bbc054ce6e235b9e7008512fed1183153e7fd368
Giao Ngo <giao.ngo@metropolia.fi>
2022-07-15 14:15:58 +0300

    Hide keyboard after text typed in EditText in AddFragment

--- app/src/main/java/fi/giao/todotask/ui/fragments/AddFragment.kt
+++ app/src/main/java/fi/giao/todotask/ui/fragments/AddFragment.kt
@@ -1,14 +1,16 @@
 package fi.giao.todotask.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
-import android.widget.Toast
-import androidx.fragment.app.viewModels
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import fi.giao.todotask.R
 import fi.giao.todotask.ToDoActivity
@@ -29,6 +31,15 @@ class AddFragment : Fragment(){
         val myAdapter = ArrayAdapter<String>(requireActivity(),android.R.layout.simple_spinner_dropdown_item,resources.getStringArray(R.array.task_priorities))
         binding.apply {
             prioritySpinner.adapter = myAdapter
+            edTxtAddTask.setOnEditorActionListener(object : TextView.OnEditorActionListener {
+                override fun onEditorAction(textView: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+                    if((actionId == EditorInfo.IME_ACTION_DONE)||(event?.keyCode  == KeyEvent.KEYCODE_ENTER) || (event?.action  == KeyEvent.ACTION_DOWN)) {
+                        val imm  = textView?.context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                        imm.hideSoftInputFromWindow(textView.windowToken,0)
+                    }
+                    return false
+                }
+            })
             addButton.setOnClickListener {
                 val taskText =  filledTaskText.editText!!.text
                 val priority = prioritySpinner.selectedItemPosition
@@ -39,5 +50,7 @@ class AddFragment : Fragment(){
         }
         return binding.root
     }
+
+
 
 }

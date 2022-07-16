a20591a3cba3519734927083261431ffb2745e11
Giao Ngo <giao.ngo@metropolia.fi>
2022-07-16 14:38:08 +0300

    Hide keyboard once user finishes typing in EditText or presses enter key. Change app label name

--- app/src/main/java/fi/giao/todotask/ui/fragments/UpdateFragment.kt
+++ app/src/main/java/fi/giao/todotask/ui/fragments/UpdateFragment.kt
@@ -1,11 +1,16 @@
 package fi.giao.todotask.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
 import androidx.navigation.NavArgs
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -30,6 +35,15 @@ class UpdateFragment : Fragment() {
         binding.apply {
             edTxtUpdateTask.setText(task.taskName)
             priorityUpdateSpinner.setSelection(task.priority)
+            edTxtUpdateTask.setOnEditorActionListener(object : TextView.OnEditorActionListener {
+                override fun onEditorAction(textView: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+                    if((actionId == EditorInfo.IME_ACTION_DONE)||(event?.keyCode  == KeyEvent.KEYCODE_ENTER) || (event?.action  == KeyEvent.ACTION_DOWN)) {
+                        val imm  = textView?.context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                        imm.hideSoftInputFromWindow(textView.windowToken,0)
+                    }
+                    return false
+                }
+            })
             updateButton.setOnClickListener {
                 val taskText = edTxtUpdateTask.text.toString()
                 val taskPriority = priorityUpdateSpinner.selectedItemPosition
